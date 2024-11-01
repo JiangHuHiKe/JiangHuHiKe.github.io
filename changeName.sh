358c64a218fcb0c49cf08e77dd445b46bda47ae0
lixiaoyi <lixiaoyi911@163.com>
2024-11-01 15:01:50 +0800

    更新其它

--- changeName.sh
+++ changeName.sh
@@ -5,10 +5,10 @@ directory="/Users/lixiaoyi/LXYFile/ResourceInGithub/JiangHuHiKe.github.io/_posts
 # directory="/Users/lxy/LXYFile/ResourceInGithub/JiangHuHiKe/_posts/2024_A_Harmony"
 
 # 遍历文件夹下文件时，跳过前面几个文件的个数
-skipCount=0
+skipCount=10
 
 # 开始日期
-start_date="2015-06-01"
+start_date="2015-06-12"
 current_date=$(date -jf "%Y-%m-%d" "$start_date" "+%Y-%m-%d")
 
 # 遍历每个文件
