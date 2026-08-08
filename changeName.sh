7dc27f19e0e3557888089dc5f6f663880aa540ef
lixiaoyi <lixiaoyi911@163.com>
2026-08-08 18:38:08 +0800

    更新状态管理

--- changeName.sh
+++ changeName.sh
@@ -3,14 +3,14 @@
 # 指定目录
 # directory="/Users/lixiaoyi/LXYFile/ResourceInGithub/JiangHuHiKe.github.io/_posts/2015_B_CodeEncrypt"
 # directory="/Users/lxy/LXYFile/ResourceInGithub/JiangHuHiKe/_posts/2024_A_Harmony"
- directory="/Users/lxy/LXYFile/ResourceInGithub/JiangHuHiKe/_posts/2022_A_Flutter"
+ directory="/Users/lixiaoyi/LXYFile/ResourceInGithub/JiangHuHiKe/_posts/2022_A_Flutter"
 
 
 # 遍历文件夹下文件时，跳过前面几个文件的个数
-skipCount=1
+skipCount=22
 
 # 开始日期
-start_date="2022-02-2"
+start_date="2022-05-01"
 current_date=$(date -jf "%Y-%m-%d" "$start_date" "+%Y-%m-%d")
 
 # 遍历每个文件
